d7d356a9bf542ce89bd24ab9a9b837f2b2bed33b
StanislavHrusanov <stanislavhrusanov@gmail.com>
2025-01-10 14:54:25 +0200

    Add routing to certificateDetails page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import styles from "./App.module.css";
 import Home from "./components/Home";
-import NavBar from "./components/NavBar";
 import CertificateDetails from "./components/CertificateDetails";
 
 function App() {
   return (
     <main className={styles.main}>
-      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route
